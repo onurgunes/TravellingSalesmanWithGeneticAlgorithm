1ea02d5815a0cb4282cadb01a7e4be9e85c2223a
onurgunes <gns.onr@gmail.com>
2015-11-29 16:27:39 +0200

    fitness calculations

--- Chromosome.py
+++ Chromosome.py
@@ -1,19 +1,32 @@
 __author__ = 'OnurGunes'
 
 import random
+from Fitness import *
 
+class Chromosome(object):
+	""" represent individual """
+
+	fitness = 0
+	genes = []
+	
+	def __init__(self, genes):
+		"""
+		"""
+		self.genes = genes
+
+	def get_fitness(self):
+		fitness = Fitness().calculate_fitness(self)
+		return fitness
 
 def generate_chromosome(genes):
     """
     generate new chromosomes
     """
 
-
 def crossover(chromosome1, chromosome2):
 	"""
 	crossover operation : swap a part of chromosome1 with a part of chromosome2
 	"""
-
 
 def mutate(chromosome):
 	"""
--- Fitness.py
+++ Fitness.py
@@ -1,15 +1,22 @@
 __author__ = 'OnurGunes'
 import Chromosome
-
+import math
 
 class Fitness(object):
     """
         Operations about fitness function
     """
 
-    def calculate_fitness(self , choromosome):
+    def calculate_fitness(self , chromosome):
         """
         Calculate chromosome' fitness
         """
+        
+        distance = 0.0
+        for index in range( len(chromosome.genes) - 1):
+        	# calculate distance between i. gene and i+1. gene
+        	distance += math.sqrt((chromosome.genes[index].latitude - chromosome.genes[index + 1].latitude)**2 
+        		+ (chromosome.genes[index].longitude - chromosome.genes[index + 1].longitude)**2)
+        return distance
         
 

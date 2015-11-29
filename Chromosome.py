c0e673a2e93e3ac0e0c4ea0ad627f780c110c652
onurgunes <gns.onr@gmail.com>
2015-11-29 16:46:14 +0200

    mutation codes added

--- Chromosome.py
+++ Chromosome.py
@@ -1,6 +1,6 @@
 __author__ = 'OnurGunes'
 
-import random
+from random import randint
 from Fitness import *
 
 class Chromosome(object):
@@ -27,9 +27,21 @@ def crossover(chromosome1, chromosome2):
 	"""
 	crossover operation : swap a part of chromosome1 with a part of chromosome2
 	"""
+	randint(0, len(chromosome1.genes))
 
 def mutate(chromosome):
 	"""
 	mutation operation : swap two genes in chromosome
 	"""
+	mutation_point_1 = 0
+	mutation_point_2 = 0
+
+	while mutation_point_1 == mutation_point_2:
+		mutation_point_1 = randint(1, len(chromosome.genes) - 1)
+		mutation_point_2 = randint(1, len(chromosome.genes) - 1)
+		
+	temp = chromosome.genes[mutation_point_1]
+	chromosome.genes[mutation_point_1] = chromosome.genes[mutation_point_2]
+	chromosome.genes[mutation_point_2] = temp
 	
+	return chromosome

309dfb9d878dbe379d2ba4daba185f7ab1c7262b
onurgunes <gns.onr@gmail.com>
2015-11-30 20:51:40 +0200

    updated fitness function

--- Fitness.py
+++ Fitness.py
@@ -11,12 +11,12 @@ class Fitness(object):
         """
         Calculate chromosome' fitness
         """
-        
+        last_gene_index = len(chromosome.genes) - 1
         distance = 0.0
-        for index in range( len(chromosome.genes) - 1):
+        for index in range(last_gene_index):
         	# calculate distance between i. gene and i+1. gene
         	distance += math.sqrt((chromosome.genes[index].latitude - chromosome.genes[index + 1].latitude)**2 
         		+ (chromosome.genes[index].longitude - chromosome.genes[index + 1].longitude)**2)
+        distance += math.sqrt((chromosome.genes[0].latitude - chromosome.genes[last_gene_index].latitude)**2 
+                + (chromosome.genes[0].longitude - chromosome.genes[last_gene_index].longitude)**2)
         return distance
-        
-

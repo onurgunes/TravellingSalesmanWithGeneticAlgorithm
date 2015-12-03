d71992f52814e0da9ed5beb24ae2c2057082206f
onurgunes <gns.onr@gmail.com>
2015-12-03 22:45:30 +0200

    bodies of function was added

--- Population.py
+++ Population.py
@@ -1,9 +1,16 @@
 __author__ = 'OnurGunes'
 
+import Chromosome
 
 class Population(object):
 
-	population_list = []
+	population = []
+	gene = []
+	
+	def __init__(self, gene):
+		"""
+		"""
+		self.gene = gene
 
 	def get_fittest(self):
 		"""
@@ -11,12 +18,33 @@ class Population(object):
 
 	def add_individual(self, chromosome):
 		"""
+		add individual to population
 		"""
+		self.population.append(chromosome)
 		
 	def delete_individual(self, index):
 		"""
+		deleete individual from population
 		"""
+		self.population.remove(chromosome)
 
 	def populate(self, limit):
 		"""
+		populate population with new chromosomes
 		"""
+		for i in xrange(limit):
+			self.population.append(Chromosome.generate_chromosome(self.gene))
+
+	def order_by_fittest(self):
+		"""
+		"""
+
+	def print_population(self):
+		for genes in self.population:
+			for gene in genes:
+				print gene.name
+			print "------------"
+
+	def delete_unfit_individuals(self, limit):
+		for index in xrange(limit, len(self.population)):
+			del self.population[index]
